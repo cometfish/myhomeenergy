db9162bb7b0562ef5c31624c28de78c746aa65bf
cometfish <me+github@cporter.dev>
2020-12-29 11:22:23 +1100

    Update upload script to work with updated (April 2018) MyHomeEnergy website Added autoupload parameter to upload script, to automatically upload multiple days with the same session Remove today and yesterday scripts - no longer working, as the session will expire if you only called it once a day Added blank favicon to prevent 404 favicon requests

--- README.md
+++ README.md
@@ -1,26 +1,37 @@
-This is a short script to pull data from Ausnet Services' MyHomeEnergy website.
+This is a short script to pull data from Ausnet Services' MyHomeEnergy website, and upload it to PVOutput.
 
 You can sign up to the MyHomeEnergy website if you are in the Ausnet Services electricity distribution zone, and you have a smart meter. You can see meter data for your electricity import, as well as solar export, and you can even enter your tariff info into the myhomeenergy website settings, which will then calculate the cost of your electricity based on time of use. (Note: the website data is not instant like an in-home display would be – it is delayed by 6 hours, but hopefully in the future this data will be more up to date.)
 
-The script pulls down the current day's, and the previous day's data (kWh and dollar cost) and displays it on a web page (for quick checks on your mobile).
-You could extend this by permanently displaying it on a small tablet or RaspberryPi/Arduino setup, to create your own in-home display!
-
-I've also included a script to upload your previous day's import and export data to PVOutput! Set it up as a cron job – remember that the myhomeenergy data is delayed, so I've set mine up to run at midday.
+The script pulls down the selected day's data (kWh) and displays it on a web page and uploads it to PVOutput.
 (Unfortunately meters don't measure Consumption so you'll have to get that figure directly from your inverter. If you're already exporting Consumption to PVOutput, delete the line starting with ‘g' => ... so that your consumption value isn't overwritten.)
 
-today.php
----------
-Open today.php in a text editor, and change the top login details to your own myhomeenergy login details. Upload to a web server, and access it to view today's usage data!
-
-upload_yesterday.php
---------------------
-Open upload_yesterday.php in a text editor, and change the top login details to your own myhomeenergy login details.
-Also change the PVOutput API key and System ID to your own.
-Upload to a web server (the path doesn't need to be accessible by web browser if you are creating a cron job), set up a cronjob for midday everyday.
+## Files
 
 date.php
---------
-Occasionally the meter data will take a few days to come up, so your 'upload_yesterday.php' won't have any data to upload.
-Upload this file to a web server, then access via /date.php?date=20140130 (date format yyyymmdd) to request a specific day's data
+---------
+Open date.php in a text editor, and change the top login details to your own MyHomeEnergy login details (your NMI number).
+Open aspxauth.txt in a text editor, and paste in your .ASPXAUTH cookie data (see below).
+Also change the PVOutput API key and System ID to your own.
+Upload this file to a web server, then access via /date.php?date=20140130 (date format yyyymmdd) to request a specific day's data.
+To upload multiple day's data, use /date.php?autoupload=65&date=20140130 (autoupload value is the seconds between each request - PVOutput has API rate limits, so you will want to set this conservatively).
 
-Created 2014-10-08
+aspxauth.txt
+-------------
+The new MyHomeEnergy website requires a CAPTCHA challenge ("I'm not a robot" prompt) on login, so it can't be logged into via an automatic script. To work around this, you can login on your browser, then copy the cookie from your session to the script, allowing the script to use your validated session.
+Follow these instructions to get your MyHomeEnergy ASPXAUTH cookie data:
+* Go to the Ausnet Services' MyHomeEnergy website
+* Press F12 to bring up the Developer Tools in your browser
+* Login to your MyHomeEnergy site
+* View the cookies sent by the page, eg.
+  * in Firefox, click the Storage tab on your Developer Tools window, then click https://www.ausnetservices.com.au under Cookies.
+* Find the cookie called ".ASPXAUTH" and copy its value (it will be a long alphanumeric value, which looks something like: "E595CAE5...C7A06D06")
+Paste the cookie value into the aspxauth.txt file, and upload it alongside the date.php file.
+The session will expire, so you will only be able to use date.php while the cookie is still valid. If using the auto-upload function, the cookie value will be refreshed with each upload, so the session should not expire until the upload has finished.
+
+## Changelog
+
+### 2014-10-08
+Created script
+
+### 2020-12-29
+Updated due to My Home Energy website update (April 2018) which added a CAPTCHA challenge on login.
--- date.php
+++ date.php
@@ -1,98 +1,152 @@
-<!doctype html>
-<html>
-<head>
-<title>Meter data for <?php echo $_GET['date']; ?></title>
-<meta name="viewport" content="width=280">
-</head>
-<body>
 <?php
 error_reporting(E_ALL);
 /** script version v1.0 **/
 /************ FILL IN YOUR MyHomeEnergy DETAILS HERE **********/
-$my_email='';
-$my_pass='';
+$aspxauthtxtfile = 'aspxauth.txt';     //paste the alphanumeric ASPXAUTH cookie data into this file
+$nmi='';
 /**************************************************************/
 
-$my_hashed_pass=hash('sha256', $my_email.':'.hash('md5', $my_pass));
-$r=makeRequest('GWRLogin','%3Cgip%3E%3Cversion%3E1%3C%2Fversion%3E%3Cemail%3E'.urlencode($my_email).'%3C%2Femail%3E%3Cpassword%3E'.$my_hashed_pass.'%3C%2Fpassword%3E%3C%2Fgip%3E');
+/************ FILL IN YOUR PVOutput DETAILS HERE **************/
+$PVOutput_APIKey = '';
+$PVOutput_SystemID = '';
+/**************************************************************/
+
+/* example usage:
+upload_date.php?date=20200131 - uploads MyHomeEnergy data from 31 Jan 2020 to PVOutput
+upload_date.php?date=20200131&autoupload=45 - uploads MyHomeEnergy data from 31 Jan 2020 to PVOutput, then progresses on to upload the next day's data 45s later (spaced out to avoid using up all your PVOutput API calls for the hour)
+*/
+$htmlheader = '<!doctype html>
+<html>
+<head>
+<title>Meter data for '.$_GET['date'].'</title>
+<link rel="icon" href="data:;base64,iVBORw0KGgo=">
+<meta name="viewport" content="width=280">';
+$htmlheadermeta = '';
+$htmlheaderend = '</head>
+<body>';
+$htmloutput = '';
+$htmlfooter = '
+</body>
+</html>';
+
+$fd=$_GET['date'];
+$td=$_GET['date'];
+$r=makeRequest($fd,$td,$nmi,$aspxauthtxtfile);
 $o=json_decode($r, true);
-//get token
-$token=$o['gip']['token'];
-
-//get meters
-$r2=makeRequest('GWRBatch', '%3Cgwrcmds%3E%3Cgwrcmd%3E%3Cgcmd%3ESPA_UserGetSmartMeterList%3C%2Fgcmd%3E%3Cgdata%3E%3Cgip%3E%3Cversion%3E1%3C%2Fversion%3E%3Ctoken%3E'.$token.'%3C%2Ftoken%3E%3C%2Fgip%3E%3C%2Fgdata%3E%3C%2Fgwrcmd%3E%3C%2Fgwrcmds%3E');
-$o=json_decode($r2, true);
-$meters=array();
-$metersr = $o['gwrcmds']['gwrcmd']['gdata']['gip']['meter'];
-for($i=0;$i<count($metersr);$i++)
-   if($metersr[$i]['status']!='-1' && $metersr[$i]['enabled']=='1')
-   {
-      $meters[]=$metersr[$i];
-	}
-
-	echo 'Day<br />';
-//request today data
-
-$fd=$_GET['date'].'000000';
-$td=date('Ymd', strtotime($_GET['date'] . ' + 1 day')).'000000';
-$req='';
-$mdata=array();
-for($i=0;$i<count($meters);$i++)
+$data=$o['Series'];
+$imported_peak=0;
+$imported_offpeak=0;
+$exported=0;
+for($i=0;$i<count($data);$i++)
 {
-   $req='%3Cgwrcmd%3E%3Cgcmd%3EDeviceGetChart%3C%2Fgcmd%3E%3Cgdata%3E%3Cgip%3E%3Cversion%3E1%3C%2Fversion%3E%3Ctoken%3E'.$token.'%3C%2Ftoken%3E%3Cdid%3E'.$meters[$i]['did'].'%3C%2Fdid%3E%3Cfd%3E'.$fd.'%3C%2Ffd%3E%3Ctd%3E'.$td.'%3C%2Ftd%3E%3Cislocal%3E1%3C%2Fislocal%3E%3Cfeed%3Eenergyperhalfhour%2Ccostperhalfhour%3C%2Ffeed%3E%3C%2Fgip%3E%3C%2Fgdata%3E%3C%2Fgwrcmd%3E';
-   $r3=makeRequest('GWRBatch', '%3Cgwrcmds%3E'.$req.'%3C%2Fgwrcmds%3E');
-$m=json_decode($r3, true);
-$mdata[]=$m['gwrcmds']['gwrcmd']['gdata']['gip']['chart'];
-   }
-//temperature data
-$req='%3Cgwrcmd%3E%3Cgcmd%3EUserGetChart%3C%2Fgcmd%3E%3Cgdata%3E%3Cgip%3E%3Cversion%3E1%3C%2Fversion%3E%3Ctoken%3E'.$token.'%3C%2Ftoken%3E%3Cfd%3E'.$fd.'%3C%2Ffd%3E%3Ctd%3E'.$td.'%3C%2Ftd%3E%3Cfeed%3Eenergyperhalfhour%2Ccostperhalfhour%2Ctempout%3C%2Ffeed%3E%3Cislocal%3E1%3C%2Fislocal%3E%3C%2Fgip%3E%3C%2Fgdata%3E%3C%2Fgwrcmd%3E';
-$r3=makeRequest('GWRBatch', '%3Cgwrcmds%3E'.$req.'%3C%2Fgwrcmds%3E');
-$tdata=json_decode($r3, true);
-
-for($i=0;$i<count($meters);$i++)
-{
-   echo $metersr[$i]['name'].': ';
-   $e=0;
-   $c=0;
-   $data =explode(',',$mdata[$i]['energyperhalfhour']);
-   for($j=1;$j<count($data);$j+=2)
-   {
-      $e+=$data[$j];
-   }
-   $data =explode(',',$mdata[$i]['costperhalfhour']);
-   for($j=1;$j<count($data);$j+=2)
-   {
-      $c+=$data[$j];
-   }
-   echo $e.' KW / $'.$c.'<br />';
+    $amt = $data[$i]['data'][0];
+    if ($data[$i]['name']=='Export Solar') {
+        $exported=$amt*-1000;
+    } else if ($data[$i]['name']=='Consumption Peak') {
+        $imported_peak=$amt*1000;
+    }  else if ($data[$i]['name']=='Consumption Off Peak') {
+        $imported_offpeak=$amt*1000;
+    }
+}
+$htmloutput .= 'Date: '.date('Y-m-d', strtotime($_GET['date'])).'<br/>';
+$htmloutput .= 'E: '.$exported.' P: '.$imported_peak.' O: '.$imported_offpeak.'<br />';
+if ($exported==0 && $imported_offpeak==0 && $imported_peak==0) {
+    $htmloutput .= 'No values returned from MyHomeEnergy';
+    printhtml($htmlheader, $htmlheadermeta, $htmlheaderend, $htmloutput, $htmlfooter);
+    return;
 }
 
-function makeRequest($cmd, $query) {
-   $url = 'https://myhomeenergy.com.au/gwr/gop.php?cmd='.$cmd.'&fmt=json&data='.$query;
+$pvouploadres = uploadDaily($PVOutput_APIKey, $PVOutput_SystemID, $_GET['date'], $exported, $imported_peak, $imported_offpeak);
+$htmloutput .= $pvouploadres;
+
+if (isset($_GET['autoupload']) && substr($pvouploadres,0,2)=='OK' && strtotime($_GET['date'])<strtotime('yesterday')) {
+    $htmlheadermeta .= '<meta http-equiv="refresh" content="'.$_GET['autoupload'].'; url=upload_date.php?autoupload='.$_GET['autoupload'].'&date='.date('Ymd', strtotime($_GET['date'].' + 1 day')).'">';
+}
+
+$htmloutput .= '<br /><br /><a href="?date='.date('Ymd', strtotime($_GET['date'].' - 1 day')).'">Previous</a> | <a href="?date='.date('Ymd', strtotime($_GET['date'].' + 1 day')).'">Next</a>';
+
+printhtml($htmlheader, $htmlheadermeta, $htmlheaderend, $htmloutput, $htmlfooter);
+
+function uploadDaily($apiKey, $systemID, $date, $exported, $imported_peak, $imported_offpeak) {
+   $url = 'http://pvoutput.org/service/r2/addoutput.jsp';
+
+   $fields = array(
+		'd' => urlencode(date('Ymd', strtotime($date))),
+		//'g' => urlencode('0'), //required if output does not already exist
+		'e' => urlencode($exported),
+		'cm' => urlencode('Imported from MyHomeEnergy'),
+		'ip' => urlencode($imported_peak),
+        'io' => urlencode($imported_offpeak)
+	);
+    $fields_string = '';
+	foreach($fields as $key=>$value)
+		$fields_string .= $key.'='.$value.'&';
+	rtrim($fields_string, '&');
    
    $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_VERBOSE, 1);
-curl_setopt($ch, CURLOPT_HEADER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER,array('Content-Type: application/x-www-form-urlencoded'));
-curl_setopt($ch, CURLOPT_REFERER, 'https://myhomeenergy.com.au/ui/GWRContent.swf?versionNumber=v2.0.318');
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-// Set so curl_exec returns the result instead of outputting it.
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    
-// Get the response and close the channel.
-$response = curl_exec($ch);
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_VERBOSE, 1);
+	curl_setopt($ch, CURLOPT_HEADER, true);
+	curl_setopt($ch, CURLOPT_HTTPHEADER,array(
+		'Content-Type: application/x-www-form-urlencoded',
+		'X-Pvoutput-Apikey: '.$apiKey,
+		'X-Pvoutput-SystemId: '.$systemID
+	));
+	// Set so curl_exec returns the result instead of outputting it.
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_POST, count($fields));
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+		
+	// Get the response and close the channel.
+	$response = curl_exec($ch);
 
-$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-$header = substr($response, 0, $header_size);
-$body = substr($response, $header_size);
-//echo 'header: '.$header;
-if(curl_errno($ch))
-   echo 'err: '.curl_error($ch);
-curl_close($ch);
-return $body;
+	$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+	$header = substr($response, 0, $header_size);
+	$body = substr($response, $header_size);
+	if(curl_errno($ch))
+	   echo 'err: '.curl_error($ch);
+	curl_close($ch);
+	return $body;
 }
 
-?>
-</body>
-</html>
+function makeRequest($startDate, $endDate, $nmi, $aspxauthtxtfile) {
+   $aspxauth=file_get_contents($aspxauthtxtfile);
+   $url = 'https://www.ausnetservices.com.au/api/Sitecore/Dashboard/GetDataForChart?customerNMI='.$nmi.'&startdate='.$startDate.'&enddate='.$endDate.'&chartView=W&typeOfData=netUsage&timeSlicesArray=[{%22START_DT%22%3A%222017-07-31%22%2C%22END_DT%22%3A%22Current%22}]&rateConFlat=0';
+   
+   $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_VERBOSE, 1);
+    curl_setopt($ch, CURLOPT_HEADER, true);
+    curl_setopt($ch, CURLOPT_REFERER, 'https://www.ausnetservices.com.au/en/myHomeEnergy/Dashboard');
+    curl_setopt($ch,CURLOPT_COOKIE, '.ASPXAUTH='.$aspxauth.';');
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    // Set so curl_exec returns the result instead of outputting it.
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        
+    // Get the response and close the channel.
+    $response = curl_exec($ch);
+
+    $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+    $header = substr($response, 0, $header_size);
+    $body = substr($response, $header_size);
+    //echo 'header: '.$header;
+    if(curl_errno($ch))
+       echo 'err: '.curl_error($ch);
+    //check if we were given a new .ASPXAUTH cookie
+    preg_match_all("/^Set-Cookie:\s+.ASPXAUTH=(.*);/mU", $header, $cookieMatchArray);
+    if (count($cookieMatchArray)>1 && count($cookieMatchArray[1])>0)
+    {
+        //save the new cookie for the next request
+        file_put_contents($aspxauthtxtfile, $cookieMatchArray[1]);
+    }
+    curl_close($ch);
+    return $body;
+}
+
+function printhtml($header, $meta, $headerend, $content, $footer) {
+    echo $header;
+    echo $meta;
+    echo $headerend;
+    echo $content;
+    echo $footer;
+}
